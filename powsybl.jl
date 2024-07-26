9c6efd155a41137d2400e2ebb615035f11e50b77
Bertrand Rix <bertrand.rix@artelys.com>
2024-07-26 14:20:12 +0200

    Basic Powsybl module with Powsybl_jll import.
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- powsybl.jl
+++ powsybl.jl
@@ -1,9 +1,9 @@
 module Powsybl
   using CxxWrap
-  library_path = joinpath(@__DIR__, "install/bin")
-  @wrapmodule(() -> joinpath(library_path, "PowsyblJlWrap"), :define_module_powsybl)
-    function __init__()
+  using Powsybl_jll
+  @wrapmodule(() -> :libPowsyblJlWrap, :define_module_powsybl)
+  function __init__()
       @initcxx
-      set_java_library_path(library_path)
+      set_java_library_path(dirname(Powsybl_jll.libmath_path))
   end
 end

8dc62f0b7a06759145f1c2765f7f04b9f31c37dd
Bertrand Rix <bertrand.rix@artelys.com>
2024-09-20 10:46:11 +0200

    Add netowk module and wrap get elements method.
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- cpp/powsybljl-cpp/powsybl_jl.cpp
+++ cpp/powsybljl-cpp/powsybl_jl.cpp
@@ -3,8 +3,54 @@
 #include "jlcxx/jlcxx.hpp"
 #include "powsybl-cpp.h"
 
+// Necessary to compile to map struct with no constructor ?
+template <> struct jlcxx::IsMirroredType<series> : std::false_type {};
+
 JLCXX_MODULE define_module_powsybl(jlcxx::Module& mod)
 {
+  mod.add_type<pypowsybl::JavaHandle>("JavaHandle");
+
+  mod.add_bits<element_type>("ElementType", jlcxx::julia_type("CppEnum"));
+  mod.set_const("BUS", element_type::BUS);
+  mod.set_const("BUS_FROM_BUS_BREAKER_VIEW", element_type::BUS_FROM_BUS_BREAKER_VIEW);
+  mod.set_const("LINE", element_type::LINE);
+  mod.set_const("TWO_WINDINGS_TRANSFORMER", element_type::TWO_WINDINGS_TRANSFORMER);
+  mod.set_const("THREE_WINDINGS_TRANSFORMER", element_type::THREE_WINDINGS_TRANSFORMER);
+  mod.set_const("GENERATOR", element_type::GENERATOR);
+  mod.set_const("LOAD", element_type::LOAD);
+  mod.set_const("BATTERY", element_type::BATTERY);
+  mod.set_const("SHUNT_COMPENSATOR", element_type::SHUNT_COMPENSATOR);
+  mod.set_const("NON_LINEAR_SHUNT_COMPENSATOR_SECTION", element_type::NON_LINEAR_SHUNT_COMPENSATOR_SECTION);
+  mod.set_const("LINEAR_SHUNT_COMPENSATOR_SECTION", element_type::LINEAR_SHUNT_COMPENSATOR_SECTION);
+  mod.set_const("DANGLING_LINE", element_type::DANGLING_LINE);
+  mod.set_const("TIE_LINE", element_type::TIE_LINE);
+  mod.set_const("LCC_CONVERTER_STATION", element_type::LCC_CONVERTER_STATION);
+  mod.set_const("VSC_CONVERTER_STATION", element_type::VSC_CONVERTER_STATION);
+  mod.set_const("STATIC_VAR_COMPENSATOR", element_type::STATIC_VAR_COMPENSATOR);
+  mod.set_const("SWITCH", element_type::SWITCH);
+  mod.set_const("VOLTAGE_LEVEL", element_type::VOLTAGE_LEVEL);
+  mod.set_const("SUBSTATION", element_type::SUBSTATION);
+  mod.set_const("BUSBAR_SECTION", element_type::BUSBAR_SECTION);
+  mod.set_const("HVDC_LINE", element_type::HVDC_LINE);
+  mod.set_const("RATIO_TAP_CHANGER_STEP", element_type::RATIO_TAP_CHANGER_STEP);
+  mod.set_const("PHASE_TAP_CHANGER_STEP", element_type::PHASE_TAP_CHANGER_STEP);
+  mod.set_const("RATIO_TAP_CHANGER", element_type::RATIO_TAP_CHANGER);
+  mod.set_const("PHASE_TAP_CHANGER", element_type::PHASE_TAP_CHANGER);
+  mod.set_const("REACTIVE_CAPABILITY_CURVE_POINT", element_type::REACTIVE_CAPABILITY_CURVE_POINT);
+  mod.set_const("OPERATIONAL_LIMITS", element_type::OPERATIONAL_LIMITS);
+  mod.set_const("MINMAX_REACTIVE_LIMITS", element_type::MINMAX_REACTIVE_LIMITS);
+  mod.set_const("ALIAS", element_type::ALIAS);
+  mod.set_const("IDENTIFIABLE", element_type::IDENTIFIABLE);
+  mod.set_const("INJECTION", element_type::INJECTION);
+  mod.set_const("BRANCH", element_type::BRANCH);
+  mod.set_const("TERMINAL", element_type::TERMINAL);
+  mod.set_const("SUB_NETWORK", element_type::SUB_NETWORK);
+
+  mod.add_bits<filter_attributes_type>("FilterAttributes", jlcxx::julia_type("CppEnum"));
+  mod.set_const("ALL_ATTRIBUTES", filter_attributes_type::ALL_ATTRIBUTES);
+  mod.set_const("DEFAULT_ATTRIBUTES", filter_attributes_type::DEFAULT_ATTRIBUTES);
+  mod.set_const("SELECTION_ATTRIBUTES", filter_attributes_type::SELECTION_ATTRIBUTES);
+
   auto preJavaCall = [](pypowsybl::GraalVmGuard* guard, exception_handler* exc){ };
   auto postJavaCall = [](){ };
   pypowsybl::init(preJavaCall, postJavaCall);
@@ -13,4 +59,42 @@ JLCXX_MODULE define_module_powsybl(jlcxx::Module& mod)
   mod.method("set_java_library_path", [] (std::string const& path) {
         pypowsybl::setJavaLibraryPath(path);
   }, "Set java.library.path JVM property");
+
+  mod.method("close_powsybl", [] () {
+          pypowsybl::closePypowsybl();
+    }, "Closes powsybl module");
+
+  mod.method("load", [] (std::string const& s) {
+    std::map<std::string, std::string> defaultParameters;
+    pypowsybl::JavaHandle network = pypowsybl::loadNetwork(s, defaultParameters, nullptr);
+    return network;
+  }, "Load a network from a file");
+
+  mod.add_type<series>("SeriesType")
+        .method("name", [](series& s) { return std::string(s.name); })
+        .method("index", [](series& s) { return (bool) s.index; })
+        .method("type", [](series& s) { return s.type; })
+        .method("as_double_array", [](series& s) {
+            return jlcxx::ArrayRef<double,1>(static_cast<double*>(s.data.ptr), s.data.length);
+         })
+        .method("as_int_array", [](series& s) {
+                  return jlcxx::ArrayRef<int,1>(static_cast<int*>(s.data.ptr), s.data.length);
+        })
+        .method("as_string_array", [](series& s) {
+                  return pypowsybl::toVector<std::string>((array *) & s.data);
+        });
+
+  mod.add_type<pypowsybl::SeriesArray>("SeriesArray")
+      .method("as_array", [](pypowsybl::SeriesArray& seriesArray) {
+          jlcxx::Array<series> data{ };
+          for(int i=0; i < seriesArray.length(); ++i) {
+            data.push_back(seriesArray.begin()[i]);
+          }
+          return data;
+      });
+
+  mod.method("create_network_elements_series_array", [] (pypowsybl::JavaHandle handle, element_type type, std::vector<std::string> const& attributes, filter_attributes_type filter_attributes, bool nominal_apparent_power, double per_unit) {
+      return pypowsybl::createNetworkElementsSeriesArray(handle, type, filter_attributes, attributes, nullptr, nominal_apparent_power, per_unit);
+      }, "Get a series");
+
 }
---
+++ src/Network.jl
@@ -0,0 +1,161 @@
+module Network
+  using ..Powsybl
+  using CxxWrap
+  using DataFrames
+
+  const nominal_apparent_power = 100.0
+  const per_unit = false
+
+  mutable struct NetworkHandle
+    handle::Powsybl.JavaHandle
+  end
+
+  function get_elements(network::NetworkHandle, type::Powsybl.ElementType, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    series_array = Powsybl.create_network_elements_series_array(network.handle, type, StdVector{StdString}(attributes), Powsybl.ALL_ATTRIBUTES, per_unit, nominal_apparent_power)
+    return create_dataframe_from_series_array(series_array[])
+  end
+
+  function get_buses(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.BUS, all_attributes, attributes)
+  end
+
+  function get_bus_breaker_view_buses(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.BUS_FROM_BUS_BREAKER_VIEW, all_attributes, attributes)
+  end
+
+  function get_generators(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.GENERATOR, all_attributes, attributes)
+  end
+
+  function get_batteries(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.BATTERY, all_attributes, attributes)
+  end
+
+  function get_lines(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.LINE, all_attributes, attributes)
+  end
+
+  function get_2_windings_transformers(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.TWO_WINDINGS_TRANSFORMER, all_attributes, attributes)
+  end
+
+  function get_3_windings_transformers(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.THREE_WINDINGS_TRANSFORMER, all_attributes, attributes)
+  end
+
+  function get_shunt_compensators(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.SHUNT_COMPENSATOR, all_attributes, attributes)
+  end
+
+  function get_non_linear_shunt_compensator_sections(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.NON_LINEAR_SHUNT_COMPENSATOR_SECTION, all_attributes, attributes)
+  end
+
+  function get_linear_shunt_compensator_sections(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.LINEAR_SHUNT_COMPENSATOR_SECTION, all_attributes, attributes)
+  end
+
+  function get_dangling_lines(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.DANGLING_LINE, all_attributes, attributes)
+  end
+
+  function get_tie_lines(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.TIE_LINE, all_attributes, attributes)
+  end
+
+  function get_lcc_converter_stations(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.LCC_CONVERTER_STATION, all_attributes, attributes)
+  end
+
+  function get_vsc_converter_stations(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.VSC_CONVERTER_STATION, all_attributes, attributes)
+  end
+
+  function get_static_var_compensators(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.STATIC_VAR_COMPENSATOR, all_attributes, attributes)
+  end
+
+  function get_voltage_levels(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.VOLTAGE_LEVEL, all_attributes, attributes)
+  end
+
+  function get_busbar_sections(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.BUSBAR_SECTION, all_attributes, attributes)
+  end
+
+  function get_substations(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.SUBSTATION, all_attributes, attributes)
+  end
+
+  function get_hvdc_lines(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.HVDC_LINE, all_attributes, attributes)
+  end
+
+  function get_switches(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.SWITCH, all_attributes, attributes)
+  end
+
+  function get_ratio_tap_changer_steps(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.RATIO_TAP_CHANGER_STEP, all_attributes, attributes)
+  end
+
+  function get_phase_tap_changer_steps(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.PHASE_TAP_CHANGER_STEP, all_attributes, attributes)
+  end
+
+  function get_ratio_tap_changers(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.RATIO_TAP_CHANGER, all_attributes, attributes)
+  end
+
+  function get_phase_tap_changers(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.PHASE_TAP_CHANGER, all_attributes, attributes)
+  end
+
+  function get_reactive_capability_curve_points(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.REACTIVE_CAPABILITY_CURVE_POINT, all_attributes, attributes)
+  end
+
+  function get_aliases(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.ALIAS, all_attributes, attributes)
+  end
+
+  function get_identifiables(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.IDENTIFIABLE, all_attributes, attributes)
+  end
+
+  function get_injections(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.INJECTION, all_attributes, attributes)
+  end
+
+  function get_branches(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.BRANCH, all_attributes, attributes)
+  end
+
+  function get_terminals(network::NetworkHandle, all_attributes::Bool = false, attributes::Vector{String} = Vector{String}())
+    return get_elements(network, Powsybl.TERMINAL, all_attributes, attributes)
+  end
+
+  function create_dataframe_from_series_array(array::Powsybl.SeriesArray)
+    myArray = Powsybl.as_array(array)
+    df = DataFrame()
+    for serie in myArray
+      type = Powsybl.type(serie)
+      name = Powsybl.name(serie)
+      if type == 0
+        data = Powsybl.as_string_array(serie)
+      elseif type == 1
+        data = Powsybl.as_double_array(serie)
+      elseif type == 2
+        data = Powsybl.as_int_array(serie)
+      else
+        continue
+      end
+      df[!, name]=data
+    end
+    return df
+  end
+
+  function load(network_file::String)::NetworkHandle
+    return NetworkHandle(Powsybl.load(network_file))
+  end
+end

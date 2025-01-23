2396ff327938cae2ffd2813995893bddd68bc00c
Bertrand Rix <bertrand.rix@artelys.com>
2025-01-23 13:38:21 +0100

    Add copyrights.
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- src/NetworkCreationUtils.jl
+++ src/NetworkCreationUtils.jl
@@ -1,3 +1,9 @@
+# Copyright (c) 2025, RTE (http://www.rte-france.com)
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+
 function _create_network(name::String, network_id::String = "")
   handle = Powsybl.create_network(name, network_id)
   return Network.NetworkHandle(handle,

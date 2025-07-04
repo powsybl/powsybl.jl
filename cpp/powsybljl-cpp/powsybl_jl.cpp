43722927defda706012a33735c837eab63b10afd
Bertrand Rix <bertrand.rix@artelys.com>
2025-07-04 10:26:13 +0200

    Basic load flow mapping.
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- cpp/powsybljl-cpp/powsybl_jl.cpp
+++ cpp/powsybljl-cpp/powsybl_jl.cpp
@@ -6,6 +6,10 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 #include <string>
+#include <functional>
+#include <memory>
+#include <iostream>
+#include <thread>
 
 #include "jlcxx/jlcxx.hpp"
 #include "powsybl-cpp.h"
@@ -13,12 +17,74 @@
 // Necessary to compile to map struct with no constructor ?
 template <> struct jlcxx::IsMirroredType<series> : std::false_type {};
 template <> struct jlcxx::IsMirroredType<network_metadata> : std::false_type {};
+template <> struct jlcxx::IsMirroredType<loadflow_component_result> : std::false_type {};
+template <> struct jlcxx::IsMirroredType<slack_bus_result> : std::false_type {};
 
 using StringStringMap = std::map<std::string, std::string>;
 
 void logFromJava(int level, long timestamp, char* loggerName, char* message) {
   //TODO Redirect log properly to julia logger
 }
+
+// Template lambda returning an attribute of a class instance
+template <typename R, typename P>
+std::function<R(P)> attribute_getter(const R P::*pm) {
+    return [pm](const P &c) -> const R & { return c.*pm; };
+}
+
+// Template lambda setting an attribute of a class instance
+template <typename R, typename P>
+std::function<void(P&, R)> attribute_setter(R P::*pm) {
+    return [pm](P &c, R v) { c.*pm = v; };
+}
+
+// Template method generating getter and setter for a given class attribute
+template <typename P, typename R>
+jlcxx::TypeWrapper<P>& map_attribute_accessor(jlcxx::TypeWrapper<P>& wrapper, std::string const& attribute_name, R P::*pm) {
+    wrapper.method(attribute_name, attribute_setter(pm));
+    wrapper.method(attribute_name, attribute_getter(pm));
+    return wrapper;
+}
+
+template <typename T>
+jlcxx::Array<T> powsybl_array_to_julia(const pypowsybl::Array<T>* parray) {
+  return powsybl_array_to_julia(parray->begin(), parray->length());
+}
+
+template <typename T>
+jlcxx::Array<T> powsybl_array_to_julia(const array* parray) {
+  return powsybl_array_to_julia((T*)parray->ptr, parray->length);
+}
+
+template <typename T>
+jlcxx::Array<T> powsybl_array_to_julia(const T* ptr, int length) {
+  jlcxx::Array<T> jlArray{ };
+  for(int i=0; i < length; ++i) {
+    jlArray.push_back(ptr[i]);
+  }
+  return jlArray;
+}
+
+// Wrap previous method in a convenient function
+// Jlcxx wrapper is accessible when we want to provide custom method / lambda
+// Simple attribute can be mapped using map_attribute (inspired by pydbind11)
+template <typename T>
+class CustomMapper {
+public:
+    CustomMapper(jlcxx::Module& module, std::string type_name) :
+     type_wrapper(module.add_type<T>(type_name)) {}
+
+    jlcxx::TypeWrapper<T> jlcxx_wrapper() {
+      return type_wrapper;
+    }
+
+    template <typename R>
+    CustomMapper<T>& method_readwrite(std::string const& attribute_name, R T::*pm) {
+        map_attribute_accessor(type_wrapper, attribute_name, pm);
+        return *this;
+    }
+    jlcxx::TypeWrapper<T> type_wrapper;
+};
 
 JLCXX_MODULE define_module_powsybl(jlcxx::Module& mod)
 {
@@ -79,13 +145,9 @@ JLCXX_MODULE define_module_powsybl(jlcxx::Module& mod)
   pypowsybl::setupLoggerCallback(reinterpret_cast<void *&>(fptr));
 
   mod.method("get_version_table", &pypowsybl::getVersionTable, "Get an ASCII table with all PowSybBl modules version");
-  mod.method("set_java_library_path", [] (std::string const& path) {
-        pypowsybl::setJavaLibraryPath(path);
-  }, "Set java.library.path JVM property");
+  mod.method("set_java_library_path", &pypowsybl::setJavaLibraryPath, "Set java.library.path JVM property");
 
-  mod.method("close_powsybl", [] () {
-          pypowsybl::closePypowsybl();
-    }, "Closes powsybl module");
+  mod.method("close_powsybl", &pypowsybl::closePypowsybl, "Closes powsybl module");
 
   mod.method("load", [] (std::string const& s, StringStringMap& parameters, std::vector<std::string>& postProcessors) {
     pypowsybl::JavaHandle network = pypowsybl::loadNetwork(s, parameters, postProcessors, nullptr);
@@ -96,17 +158,9 @@ JLCXX_MODULE define_module_powsybl(jlcxx::Module& mod)
     return pypowsybl::createNetwork(name, id);
   }, "create an example network");
 
-  mod.method("get_network_available_post_processors", [] () {
-      return pypowsybl::getNetworkImportPostProcessors();
-    }, "Get available post processors");
-
-  mod.method("get_network_import_formats", [] () {
-        return pypowsybl::getNetworkImportFormats();
-      }, "Get available import format");
-
-  mod.method("get_network_export_formats", [] () {
-          return pypowsybl::getNetworkExportFormats();
-        }, "Get available export format");
+  mod.method("get_network_available_post_processors", &pypowsybl::getNetworkImportPostProcessors, "Get available post processors");
+  mod.method("get_network_import_formats", &pypowsybl::getNetworkImportFormats, "Get available import format");
+  mod.method("get_network_export_formats", &pypowsybl::getNetworkExportFormats, "Get available export format");
 
   mod.method("save_network", [] (pypowsybl::JavaHandle handle, std::string const& file, std::string const& format, StringStringMap const& parameters) {
       pypowsybl::saveNetwork(handle, file, format, parameters, nullptr);
@@ -148,11 +202,7 @@ JLCXX_MODULE define_module_powsybl(jlcxx::Module& mod)
 
   mod.add_type<pypowsybl::SeriesArray>("SeriesArray")
       .method("as_array", [](pypowsybl::SeriesArray& seriesArray) {
-          jlcxx::Array<series> data{ };
-          for(int i=0; i < seriesArray.length(); ++i) {
-            data.push_back(seriesArray.begin()[i]);
-          }
-          return data;
+          return powsybl_array_to_julia(&seriesArray);
       });
 
   mod.method("create_network_elements_series_array", [] (pypowsybl::JavaHandle handle, element_type type, std::vector<std::string> const& attributes, filter_attributes_type filter_attributes, bool nominal_apparent_power, double per_unit) {
@@ -167,4 +217,96 @@ JLCXX_MODULE define_module_powsybl(jlcxx::Module& mod)
           return pypowsybl::getExtensionsNames();
           }, "Get all the extensions names available");
 
+  // VoltageInitMode
+  mod.add_bits<pypowsybl::VoltageInitMode>("VoltageInitMode", jlcxx::julia_type("CppEnum"));
+  mod.set_const("UNIFORM_VALUES", pypowsybl::VoltageInitMode::UNIFORM_VALUES);
+  mod.set_const("PREVIOUS_VALUES", pypowsybl::VoltageInitMode::PREVIOUS_VALUES);
+  mod.set_const("DC_VALUES", pypowsybl::VoltageInitMode::DC_VALUES);
+
+  // ConnectedComponentMode
+  mod.add_bits<pypowsybl::ConnectedComponentMode>("ConnectedComponentMode", jlcxx::julia_type("CppEnum"));
+  mod.set_const("ALL", pypowsybl::ConnectedComponentMode::ALL);
+  mod.set_const("MAIN", pypowsybl::ConnectedComponentMode::MAIN);
+
+  // BalanceType
+  mod.add_bits<pypowsybl::BalanceType>("BalanceType", jlcxx::julia_type("CppEnum"));
+  mod.set_const("PROPORTIONAL_TO_GENERATION_P", pypowsybl::BalanceType::PROPORTIONAL_TO_GENERATION_P);
+  mod.set_const("PROPORTIONAL_TO_GENERATION_P_MAX", pypowsybl::BalanceType::PROPORTIONAL_TO_GENERATION_P_MAX);
+  mod.set_const("PROPORTIONAL_TO_GENERATION_REMAINING_MARGIN", pypowsybl::BalanceType::PROPORTIONAL_TO_GENERATION_REMAINING_MARGIN);
+  mod.set_const("PROPORTIONAL_TO_GENERATION_PARTICIPATION_FACTOR", pypowsybl::BalanceType::PROPORTIONAL_TO_GENERATION_PARTICIPATION_FACTOR);
+  mod.set_const("PROPORTIONAL_TO_LOAD", pypowsybl::BalanceType::PROPORTIONAL_TO_LOAD);
+  mod.set_const("PROPORTIONAL_TO_CONFORM_LOAD", pypowsybl::BalanceType::PROPORTIONAL_TO_CONFORM_LOAD);
+
+  // LoadFlowComponentStatus
+  mod.add_bits<pypowsybl::LoadFlowComponentStatus>("LoadFlowComponentStatus", jlcxx::julia_type("CppEnum"));
+  mod.set_const("CONVERGED", pypowsybl::LoadFlowComponentStatus::CONVERGED);
+  mod.set_const("FAILED", pypowsybl::LoadFlowComponentStatus::FAILED);
+  mod.set_const("MAX_ITERATION_REACHED", pypowsybl::LoadFlowComponentStatus::MAX_ITERATION_REACHED);
+  mod.set_const("NO_CALCULATION", pypowsybl::LoadFlowComponentStatus::NO_CALCULATION);
+
+  mod.add_type<slack_bus_result>("SlackBusResult")
+          .method("id", [](const slack_bus_result& r) {
+             return std::string(r.id);
+          })
+          .method("active_power_mismatch", [](const slack_bus_result& r) {
+             return r.active_power_mismatch;
+          });
+
+  mod.add_type<loadflow_component_result>("LoadFlowComponentResult")
+          .method("connected_component_num", [](const loadflow_component_result& r) {
+             return r.connected_component_num;
+          })
+          .method("synchronous_component_num", [](const loadflow_component_result& r) {
+             return r.synchronous_component_num;
+          })
+          .method("status", [](const loadflow_component_result& r) {
+             return static_cast<pypowsybl::LoadFlowComponentStatus>(r.status);
+          })
+          .method("status_text", [](const loadflow_component_result& r) {
+             return std::string(r.status_text);
+          })
+          .method("iteration_count", [](const loadflow_component_result& r) {
+             return r.iteration_count;
+          })
+          .method("reference_bus_id", [](const loadflow_component_result& r) {
+             return std::string(r.reference_bus_id);
+          })
+          .method("distributed_active_power", [](const loadflow_component_result& r) {
+             return r.distributed_active_power;
+          })
+          .method("slack_bus_results", [](const loadflow_component_result& r) {
+             return powsybl_array_to_julia<slack_bus_result>(&(r.slack_bus_results));
+          });
+
+  CustomMapper<pypowsybl::LoadFlowParameters> lfParametersMapper(mod, "LoadFlowParameters");
+  lfParametersMapper.jlcxx_wrapper()
+     .constructor([] () {
+       return pypowsybl::createLoadFlowParameters();
+    });
+  lfParametersMapper
+    .method_readwrite("voltage_init_mode", &pypowsybl::LoadFlowParameters::voltage_init_mode)
+    .method_readwrite("transformer_voltage_control_on", &pypowsybl::LoadFlowParameters::transformer_voltage_control_on)
+    .method_readwrite("use_reactive_limits", &pypowsybl::LoadFlowParameters::use_reactive_limits)
+    .method_readwrite("phase_shifter_regulation_on", &pypowsybl::LoadFlowParameters::phase_shifter_regulation_on)
+    .method_readwrite("twt_split_shunt_admittance", &pypowsybl::LoadFlowParameters::twt_split_shunt_admittance)
+    .method_readwrite("shunt_compensator_voltage_control_on", &pypowsybl::LoadFlowParameters::shunt_compensator_voltage_control_on)
+    .method_readwrite("read_slack_bus", &pypowsybl::LoadFlowParameters::read_slack_bus)
+    .method_readwrite("write_slack_bus", &pypowsybl::LoadFlowParameters::write_slack_bus)
+    .method_readwrite("distributed_slack", &pypowsybl::LoadFlowParameters::distributed_slack)
+    .method_readwrite("balance_type", &pypowsybl::LoadFlowParameters::balance_type)
+    .method_readwrite("dc_use_transformer_ratio", &pypowsybl::LoadFlowParameters::dc_use_transformer_ratio)
+    .method_readwrite("countries_to_balance", &pypowsybl::LoadFlowParameters::countries_to_balance)
+    .method_readwrite("connected_component_mode", &pypowsybl::LoadFlowParameters::connected_component_mode)
+    .method_readwrite("dc_power_factor", &pypowsybl::LoadFlowParameters::dc_power_factor)
+    .method_readwrite("provider_parameters_keys", &pypowsybl::LoadFlowParameters::provider_parameters_keys)
+    .method_readwrite("provider_parameters_values", &pypowsybl::LoadFlowParameters::provider_parameters_values);
+
+  mod.method("run_load_flow", [] (const pypowsybl::JavaHandle& network, const pypowsybl::LoadFlowParameters& parameters, bool dc, const std::string& provider) {
+                pypowsybl::LoadFlowComponentResultArray* results = pypowsybl::runLoadFlow(network, dc, parameters, provider, nullptr);
+                return powsybl_array_to_julia(results);
+      }, "Run and AC load flow");
+
+  mod.method("create_loadflow_provider_parameters_series_array", [] (const std::string& provider) {
+            return pypowsybl::createLoadFlowProviderParametersSeriesArray(provider);
+    }, "Create a parameters series array for a given loadflow provider");
 }

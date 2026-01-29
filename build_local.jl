9cd71bf06f730be3b83664b1ac841b50e581ce3c
Bertrand Rix <bertrand.rix@artelys.com>
2026-01-29 11:00:06 +0100

    Bump pypowsybl to 1.14.0
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- build_local.jl
+++ build_local.jl
@@ -9,7 +9,7 @@ using BinaryBuilder, Pkg
 name = "Powsybl"
 version = v"0.2.0"
 
-pypowsybl_version = v"1.12.0"
+pypowsybl_version = v"1.14.0"
 
 sources = [
     DirectorySource("./cpp", target="cpp"),
